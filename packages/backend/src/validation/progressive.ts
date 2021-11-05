22cce5f74dd6dae5e0ae1cbc4000f9c7d73f9e9f
Jackson Graves <jackson.graves@me.com>
2021-11-04 21:56:59 -0500

    Finished scaffolding basic backend

--- packages/backend/src/validation/progressive.ts
+++ packages/backend/src/validation/progressive.ts
@@ -1,5 +1,21 @@
-export const validateProgressive = (username: string, password: string) => {
+import axios from "axios";
+
+/**
+ * @function validateProgressive
+ * @description Attempts to validate the user credentials for the Progressive workflow
+ * @param username String corresponding to the User ID
+ * @param password user's password
+ * @throws {Error}
+ * @returns {void}
+ */
+export const validateProgressive = (username: string, password: string): void => {
 	console.log("noOp");
 	console.log("Attempting to validate Progressive");
 	console.log(`User ID: ${username} Password: ${password}`);
+
+	try {
+		// TODO perform validation workflow
+	} catch (error) {
+		throw error?.message || error || "Unknown error";
+	}
 };
